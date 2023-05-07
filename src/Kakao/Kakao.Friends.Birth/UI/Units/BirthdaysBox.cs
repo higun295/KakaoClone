2ce23ed77271130092cd7388eadd7ebc18108c67
higun295 <higun295@gmail.com>
2023-05-07 22:28:08 +0900

    20230507_4

---
+++ src/Kakao/Kakao.Friends.Birth/UI/Units/BirthdaysBox.cs
@@ -0,0 +1,14 @@
+﻿using Kakao.LayoutSupport.UI.Units;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Kakao.Friends.Birth.UI.Units
+{
+    public class BirthdaysBox : FriendsBox
+    {
+    }
+}
